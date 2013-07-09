265c0b699b54ab37487d691bbcf2a1c4fa7aaa72
Mester Balázs <balazs.mester@gmail.com>
2013-07-09 06:33:34 +0200

    Rails added.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Integer2numberString::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Integer2numberString::Application.config.secret_token = 'a1e8534ccf2ed9e183a200da1617d3fe3e2960710b49500562f562eb2ecc23d4f543696fa2a717767321503d9152fd8fdde044e5f9f4f9d3f7975fba2322a6b9'
